9caed7e414c3aa3041dbe220e32ef79e431eb493
Bhupinder <bhupinderkarnawal@gmail.com>
2025-01-26 20:02:26 +0530

    new alien mobile

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,3 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
 })
-
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     rollupOptions: {
-//       external: ['react-router-dom'],
-//     },
-//   },
-// });
