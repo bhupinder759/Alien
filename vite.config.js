6139fbac61317412e8eb0252650b80cb859a5178
Bhupinder <bhupinderkarnawal@gmail.com>
2025-01-26 19:18:15 +0530

    new website

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,20 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react()],
+})
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
 // export default defineConfig({
 //   plugins: [react()],
-// })
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
-    },
-  },
-});
+//   build: {
+//     rollupOptions: {
+//       external: ['react-router-dom'],
+//     },
+//   },
+// });
